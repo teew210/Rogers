f66cfeb2bc3246c30c469f5b077f12a243ed0271
teew210 <140025291+teew210@users.noreply.github.com>
2023-07-20 00:03:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_gq7an17", "template_xe7frwd", formData)
+    emailjs.send("service_a48qc3m", "template_v3zgs5l", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
